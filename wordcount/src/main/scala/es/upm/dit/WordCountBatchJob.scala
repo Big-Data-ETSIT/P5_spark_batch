ce70fe7bec5ec53072bcbe67eec5c50c6e6fd7d0
javicond3 <javier.conde.diaz@upm.es>
2023-03-27 20:28:28 +0200

    first version

---
+++ wordcount/src/main/scala/es/upm/dit/WordCountBatchJob.scala
@@ -0,0 +1,52 @@
+package es.upm.dit
+
+import org.apache.spark.sql.{SparkSession, SaveMode}
+import java.io.File
+
+object WordCountBatchJob{
+  final val BASE_PATH = "../../P5_spark_batch"
+  def main(args: Array[String]) {
+    // create SparkSession
+    val spark = SparkSession
+    .builder
+    .appName("WordCountBatchJob")
+    .master("local[*]")
+    .getOrCreate()
+    
+    spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+
+    import spark.implicits._
+
+
+    val folderPath = BASE_PATH + "/books"
+    val resultFolder = BASE_PATH + "/result"
+
+
+    val textFiles = spark
+    .sparkContext
+    .wholeTextFiles(folderPath)
+    .map(_._2)
+
+    // count words in each file and clean words
+    val wordCounts = textFiles.flatMap(line => line.split(" "))
+                             .map(word => (
+                                word
+                                  .toLowerCase()
+                                  .replaceAll("[^a-zA-Z0-9]", ""), 
+                                1))
+                             .reduceByKey(_ + _)
+                             .sortBy(_._2, ascending = false)
+
+    // save word to dataframe and then to CSV file
+    wordCounts.toDF("Word", "Count")
+              .coalesce(1) //in a single file, not recommended for huge csv files
+              .write
+              .mode(SaveMode.Overwrite)
+              .option("header", "true")
+              .format("csv")
+              .save(resultFolder)
+
+    // stop SparkSession
+    spark.stop()
+  }
+}
